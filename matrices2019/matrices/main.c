53932ad6099c9932d534fdb991b7e3bf1f8a7438
Lorena <karla1234>
2019-08-22 18:03:15 -0300

    Matrices

---
+++ matrices2019/matrices/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void ejercicio1();
+int main()
+{
+    printf("Hello world!\n");
+
+    ejercicio1();
+
+
+    return 0;
+}
+
+void ejercicio1()
+{
+    printf("adios");
+}
