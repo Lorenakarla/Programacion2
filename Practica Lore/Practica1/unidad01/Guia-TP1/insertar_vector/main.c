0690ae2f6187dc6993e65d003be1459074692689
lorenakarla <karla.lorena70@gmail.com>
2019-08-29 17:50:52 -0300

    ejercicio: insertar en vector

---
+++ Practica Lore/Practica1/unidad01/Guia-TP1/insertar_vector/main.c
@@ -0,0 +1,13 @@
+///Insertar un elemento en un vector dada la posicion
+
+#include <stdio.h>
+#include <stdlib.h>
+void insertar(int v[], int val);
+void mostrar_vector(int v[]);
+
+int main()
+{
+    int vec[]={1,2,4,5,6};
+    printf("Ingrese una posicion: ");
+    return 0;
+}
